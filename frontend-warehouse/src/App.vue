ffe5d10677e6312433eb1b1e1721ed5077b29f76
MichalThx <rsshultaj@gmail.com>
2022-05-01 00:01:35 +0200

    Fixing nested gits

---
+++ frontend-warehouse/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  
+  <h1> HELOO </h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  mounted() {
+    console.log("mounted");
+    this.getList();
+  },
+  methods: {
+    getList() {
+      this.axios.get("http://localhost:8080/warehouse/list").then((response) => {
+        console.log(response.data);
+      })
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
